cb7c9603092106b43b1c8fef3bc110a9b5396ac4
Pirre Gouvea, Andre <pirre@amazon.com>
2020-04-06 02:42:47 -0700

    add az.json

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-"strict mode";
+'use strict';
 
-
-class HttpClient {
-    constructor() {
-        this.get = function (aUrl, aCallback) {
-            var anHttpRequest = new XMLHttpRequest();
-            anHttpRequest.onreadystatechange = function () {
-                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                    aCallback(anHttpRequest.responseText);
-            };
-            anHttpRequest.open("GET", aUrl, true);
-            anHttpRequest.send(null);
-        };
-    }
-}
-
-
-let client = new HttpClient();
-client.get('http://127.0.0.1:59333/', function(response) {
-   let el = document.getElementById("root");
-   el.innerHTML = response;
+$(document).ready(function (){
+    $.ajax({
+        type: "GET",
+        url: "az.json",
+        dataType: "text",
+        success: function(data) {processData(data);}
+    });
 });
+
+function processData(data){
+    let elout = document.getElementById("output");
+    elout.insertAdjacentHTML('beforeend', data);
+}
